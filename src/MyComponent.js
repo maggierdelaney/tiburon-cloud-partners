0507b06f2908f62ce3822b3b73f325bca259f9c4
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2024-09-21 10:48:11 -0500

    Initial commit

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyComponent.css'; // Import the CSS file
+
+const MyComponent = () => {
+    return (
+        <div className="my-component"> {/* Apply the class here */}
+            <h1>Tiburon Cloud Partners</h1>
+        </div>
+    );
+};
+
+export default MyComponent;
